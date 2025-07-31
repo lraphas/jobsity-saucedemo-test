e8886eeab598c658399edc434f591a6f7a740d43
Lucas Raphael <lraphael@collectors.com>
2025-07-31 11:24:04 -0300

    Updated login and inventory tests to use users fixture file.
    Updated login and inventory tests to use users fixture file.

--- tests/inventory.spec.js
+++ tests/inventory.spec.js
@@ -1,9 +1,11 @@
 import { test } from '@playwright/test';
+import usersData from '../test-data/users.json';
 import { LoginPage } from '../pages/LoginPage';
 import { InventoryPage } from '../pages/InventoryPage';
 
 test.describe('Inventory Tests - SauceDemo', () => {
   let loginPage;
+  let user;
   let inventoryPage;
 
   test.beforeEach(async ({ page }) => {
@@ -13,13 +15,17 @@ test.describe('Inventory Tests - SauceDemo', () => {
   });
 
   test('Check unique Inventory images with Standard User', async () => {
-    await loginPage.login('standard_user', 'secret_sauce');
+    user = usersData.standard_user;
+
+    await loginPage.login(user.username, user.password);
     await inventoryPage.assertOnInventoryPage();
     await inventoryPage.assertImagesAreUnique();
   });
 
   test('Check unique Inventory images with Problem User', async () => {
-    await loginPage.login('problem_user', 'secret_sauce');
+    user = usersData.problem_user;
+
+    await loginPage.login(user.username, user.password);
     await inventoryPage.assertOnInventoryPage();
     await inventoryPage.assertImagesAreUnique();
   });
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,9 +1,11 @@
 import { test } from '@playwright/test';
+import usersData from '../test-data/users.json';
 import { LoginPage } from '../pages/LoginPage';
 import { InventoryPage } from '../pages/InventoryPage';
 
 test.describe('Login Tests - SauceDemo', () => {
   let loginPage;
+  let user;
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
@@ -12,13 +14,16 @@ test.describe('Login Tests - SauceDemo', () => {
 
   test('Successful Login', async ({ page }) => {
     const inventoryPage = new InventoryPage(page);
+    user = usersData.standard_user;
 
-    await loginPage.login('standard_user', 'secret_sauce');
+    await loginPage.login(user.username, user.password);
     await inventoryPage.assertOnInventoryPage();
   });
 
   test('Locked out User', async () => {
-    await loginPage.login('locked_out_user', 'secret_sauce');
+  user = usersData.locked_out_user;
+
+    await loginPage.login(user.username, user.password);
     await loginPage.assertLoginError('Epic sadface: Sorry, this user has been locked out.');
   });
 });
